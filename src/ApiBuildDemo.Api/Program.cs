e7ce5eb3cf20f56eff479e0093c87c130a30a74f
Pere Zamora <pzamgar@gmail.com>
2019-08-11 15:07:34 +0200

    :whale: Serilog and Seq

--- src/ApiBuildDemo.Api/Program.cs
+++ src/ApiBuildDemo.Api/Program.cs
@@ -11,32 +11,51 @@ using Serilog;
 
 namespace ApiBuildDemo.Api {
     public class Program {
-        public static void Main (string[] args) {
-            CreateWebHostBuilder (args).Build ().Run ();
+        public static int Main (string[] args) {
+
+            var configuration = GetConfiguration ();
+
+            Log.Logger = CreateSerilogLogger (configuration);
+
+            try {
+                Log.Information ("Configuring web host ");
+                var host = BuildWebHost (configuration, args);
+                Log.Information ("Starting web host ...");
+                host.Run ();
+
+                return 0;
+            } catch (Exception ex) {
+                Log.Fatal (ex, "Program terminated unexpectedly ");
+                return 1;
+            } finally {
+                Log.CloseAndFlush ();
+            }
+
+        }
+        private static IWebHost BuildWebHost (IConfiguration configuration, string[] args) =>
+            WebHost.CreateDefaultBuilder (args)
+            .UseStartup<Startup> ()
+            .UseContentRoot (Directory.GetCurrentDirectory ())
+            .UseConfiguration (configuration)
+            .UseSerilog ()
+            .Build ();
+
+        private static Serilog.ILogger CreateSerilogLogger (IConfiguration configuration) {
+            var seqServerUrl = configuration["Serilog:SeqServerUrl"];
+            return new LoggerConfiguration ()
+                .MinimumLevel.Verbose ()
+                .WriteTo.Seq (string.IsNullOrWhiteSpace (seqServerUrl) ? "http://seq" : seqServerUrl)
+                .ReadFrom.Configuration (configuration)
+                .CreateLogger ();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder (string[] args) {
+        private static IConfiguration GetConfiguration () {
+            var builder = new ConfigurationBuilder ()
+                .SetBasePath (Directory.GetCurrentDirectory ())
+                .AddJsonFile ("appsettings.json", optional : false, reloadOnChange : true)
+                .AddEnvironmentVariables ();
 
-            var webHost = new WebHostBuilder ()
-                .UseKestrel ()
-                .UseContentRoot (Directory.GetCurrentDirectory ())
-                .ConfigureAppConfiguration ((hostingContext, config) => {
-                    var env = hostingContext.HostingEnvironment;
-                    config.AddJsonFile ("appsettings.json", optional : true, reloadOnChange : true)
-                        .AddJsonFile ($"appsettings.{env.EnvironmentName}.json",
-                            optional : true, reloadOnChange : true);
-                    config.AddEnvironmentVariables ();
-                })
-                .ConfigureLogging ((hostingContext, logging) => {
-                     Log.Logger = new LoggerConfiguration ()
-                        .ReadFrom.Configuration (hostingContext.Configuration)
-                        .CreateLogger ();
-                    Log.Information ("Starting web host");
-                })
-                .UseSerilog ()
-                .UseStartup<Startup> ();
-
-            return webHost;
+            return builder.Build ();
         }
     }
 }
